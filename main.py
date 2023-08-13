0268e5369edfa802fbdf24ac30d667ed055f99f6
denielfer <dfc152@gmail.com>
2023-08-13 16:53:39 -0300

    ajustando main pra gerar arquivos de saida

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import util
+import analizador_lexico_recurcivo
 paths = util.load_files()
 if not paths:
     print('Nenhum arquivo encontrado')
@@ -7,5 +8,12 @@ for path in paths:
     with open(path,'r') as file:
         with open(path.replace("input",'output',1)
                   .replace('.txt','-saida.txt'),'w') as out_file:
-            for char in "".join(file.readlines()):
-                out_file.write(char+'\n')
+            for n,line in enumerate(file.readlines()):
+                line = line[:-1] if line[-1] == '-1' else line
+                try:
+                    for token in analizador_lexico_recurcivo.get_token(line,
+                                                        analizador_lexico_recurcivo.prioridade,
+                                                        analizador_lexico_recurcivo.comportamentos):
+                        out_file.write(f'{n} <{analizador_lexico_recurcivo.codigos[token[0]]},{token[1]}>\n')
+                except analizador_lexico_recurcivo.comentario_linha_excption:
+                    pass               
