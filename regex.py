2de5a05442e412e4112c02196d7d44cc4bd5f316
João Pedro Rios Carvalho <jprcarvalho1@gmail.com>
2023-08-15 15:09:50 -0300

    add test on regex

--- regex.py
+++ regex.py
@@ -1,3 +1,24 @@
+import re
+
+txt = 'sadasd.dasd'
+
+print('PRE:')
+print(re.findall(r'\b(variables|const|class|methods|objects|main|return|if|else|then|for|read|print|void|int|real|boolean|string|true|false)\b', txt))
+print('NRO:')
+print(re.findall(r'\b\d+\.?\d*\b', txt))
+print('IDE:')
+print(re.findall(r'\b[a-zA-Z]{1}\w*[^(\.\s)]\b', txt))
+print('CAC:')
+print(re.findall(r'\".*\"', txt))
+print('ART:')
+print(re.findall(r'\+\+|--|[+*/-]', txt))
+print('REL:')
+print(re.findall(r'!=|==|<=|>=|[=<>]', txt))
+print('LOG:')
+print(re.findall(r'!|&&|\|\|', txt))
+print('DEL:')
+print(re.findall(r';|,|\.|\(|\)|\[|\]|\{|\}|->', txt))
+
 # RegEx possíveis (use with findall)
 
 # r'\b(variables|const|class|methods|objects|main|return|if|else|then|for|read|print|void|int|real|boolean|string|true|false)\b' PRE
